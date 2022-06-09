a98af2ce94a77e2863d7e0902b14ecb92f0c22f9
Prokop <prokop.schield@protonmail.com>
2022-06-09 18:57:39 +0200

    feat(src/jobqueue.ts): implement queue

--- src/jobqueue.ts
+++ src/jobqueue.ts
@@ -1,5 +1,10 @@
+import { ProceduralQueue } from 'ps-std';
+
 import { RedisClientType } from '.';
 import { logger } from './logger';
+import { screenshot } from './screenshot';
+
+export const queue = new ProceduralQueue(screenshot);
 
 export const processJob = async (redis: RedisClientType, task: string) => {
     logger.debug('processingJob', task);
