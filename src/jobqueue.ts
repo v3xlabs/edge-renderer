60f917c368cbc566ae6d8149ffdac2fae5b7a46e
Svemat01 <jakob.helgesson@gmail.com>
2022-06-12 12:51:53 +0200

    Implement screenshot into queue

--- src/jobqueue.ts
+++ src/jobqueue.ts
@@ -1,10 +1,29 @@
 import { RedisClientType } from '.';
 import { logger } from './logger';
+import { screenshot } from './screenshot';
 
 export const processJob = async (redis: RedisClientType, task: string) => {
     logger.debug('processingJob', task);
 
-    await new Promise<void>((accumulator) => {
-        setTimeout(accumulator, 2000);
-    });
+    let url: URL;
+
+    try {
+        // Will fail if not a valid url
+        url = new URL(task);
+    } catch {
+        logger.error('Invalid task', task);
+
+        return;
+    }
+
+    const images = await screenshot(task);
+
+    logger.debug('images', images);
+
+    for (const [resolution, data] of Object.entries(images)) {
+        redis.hSet(`images:${url.hostname}`, resolution, data);
+    }
+
+    // Expire after x amount of time
+    // redis.expire(`images:${url.hostname}`, 10);
 };
